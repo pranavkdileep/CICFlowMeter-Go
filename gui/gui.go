f0bfaa80ee1c46c88ef98fade930e9374f67af33
pranavkdileep <pranavdileep10@gmail.com>
2026-02-04 12:03:23 +0530

    working with map

--- gui/gui.go
+++ gui/gui.go
@@ -3,14 +3,14 @@ package gui
 import (
 	"client/utils"
 	"math/rand"
-	"time"
+	//"time"
 )
 
 
 
 func PreditAttack(flow utils.Flow) string {
-	delay := time.Millisecond * time.Duration(rand.Intn(500)+100)
-	time.Sleep(delay)
+	//delay := time.Millisecond * time.Duration(rand.Intn(500)+100)
+	//time.Sleep(delay)
 	attacks := []string{"BENIGN", "DrDoS_DNS", "DrDoS_LDAP", "DrDoS_MSSQL", "DrDoS_NTP", "DrDoS_NetBIOS", "DrDoS_SNMP", "DrDoS_SSDP", "DrDoS_UDP", "Syn", "TFTP", "UDP-lag", "WebDDoS"}
 	return attacks[rand.Intn(len(attacks))]
 }
